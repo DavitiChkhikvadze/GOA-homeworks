24d6334832edbfbd22012586249b9abc23ee3cee
DavitiChkhikvadze <data.poshki2020@gmail.com>
2024-02-16 15:44:55 +0400

    the seventh commit

---
+++ day 04/classwork/day 04.py
@@ -0,0 +1,8 @@
+my_name="Daviti"
+print(my_name)
+#my name is a variable name
+# = არის მიმნიჭებელი ოპერატორი
+# Daviti არის ცვლადის მნიშვნელობა
+x=5
+y=10 
+print (x + y)
---
+++ day 04/homework/lesson4.py
@@ -0,0 +1,6 @@
+price = 269
+balance = 742
+tax = 2
+print(balance - price * tax)
+print(balance / tax + price)
+print(balance / tax - price)
