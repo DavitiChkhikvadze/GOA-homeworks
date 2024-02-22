15d6e23c02324762adbb8df03ebac1cd36657828
DavitiChkhikvadze <data.poshki2020@gmail.com>
2024-02-22 22:26:46 +0400

    the nineth commit

---
+++ day 06/classwork/lesson 6.py
@@ -0,0 +1,15 @@
+# name = input("please enter your name")
+# lastname = input("please enter your last name")
+# age = input("please enter your age")
+# print("put your imputs here:", name, lastname, age)
+
+# print(int("20") + 10)
+
+number1 = int(input("please enter your first number:"))
+number2 = int(input("please enter your second number:"))
+number3 = int(input("please enter your third number:"))
+
+print(number1  + number2 * number3)
+
+
+
