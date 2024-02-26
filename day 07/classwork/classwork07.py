7c22def53ce920974ea4b912fb211383a616be70
DavitiChkhikvadze <data.poshki2020@gmail.com>
2024-02-26 15:20:23 +0400

    the tenth commit

---
+++ day 07/classwork/classwork07.py
@@ -0,0 +1,27 @@
+# დავალება 1
+
+name = input('please enter your name: ')
+surname = input('please enter your surname: ')
+age = input('please enter your age: ')
+
+print(name + " " + surname + " " + age)
+
+
+#დავალება 2
+
+num1 = input('please enter your first number: ')
+num2 = input('please enter your second number: ')
+
+print(num1 < num2)
+print(num1 > num2)
+print(num1 == num2)
+
+
+#გატესტვა
+
+print(int("46"))
+print(int(6.5))
+print(str(943))
+print(float("50"))
+print(type("myname"))
+print(type(True))
---
+++ day 07/homework/homework_07.py
@@ -0,0 +1,45 @@
+#პარასკევს განხილული საკითხების გატესტვა (int(), str(), float(), type(), <,>,==, 
+#true or/and false)
+
+num1 = 56
+num2 = 32
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
+print(str(num1) + str(num2))
+print(float(num1) + num2)
+
+flat1 = "56"
+flat2 = 8.3
+
+print(int(flat1) + flat2)
+print(float(flat1) + flat2)
+
+print(type(num1))
+print(type(num2))
+print(type(flat1))
+print(type(flat2))
+
+print(True or True)
+print(False or False)
+print(True or False)
+print(True and True)
+print(False and False)
+print(True and False)
+
+print(type(True))
+print(type(False))
+
+number1 = input("please enter your first number: ")
+number2 = input("please enter your second number: ")
+
+print(type(number1))
+print(type(number2))
+print(float(number1) + int(number2) )
+print(number1 > number2)
+print(number1 < number2)
+print(number1 == number2)
+
+#მგონი ყველაფერი გავტესტე. იმედია მოგეწონებათ შესრულებული დავალება :D
