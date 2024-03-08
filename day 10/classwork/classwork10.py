8bf719340829049787eadf89915df6e3c310a149
DavitiChkhikvadze <data.poshki2020@gmail.com>
2024-03-08 16:03:42 +0400

    the fourteenth commit

---
+++ day 10/classwork/classwork10.py
@@ -0,0 +1,35 @@
+
+print(range(7, 22))
+print(range(10, 21))
+print(range(0, 10))
+
+for i in range(11):
+    print(i)
+
+for i in range(20):
+    print(i)
+
+for i in range(10):
+    print("Daviti")
+
+for i in range(1,31):
+    print(i)
+
+for i in range(1, 11):
+    print(i * i)
+
+result = 0
+for i in range(1,11):
+    result = result + 1
+
+print(result)
+
+for i in range(1, 11):
+    print(i / 2)
+
+result1 = 1
+for i in range(1, 11):
+    result1 = result1 * i
+
+print(result1)
+
---
+++ day 10/homework/homework10.py
@@ -0,0 +1,42 @@
+for i in range(21):
+    print(i)
+
+num1 = int(input("please enter your 1st number: "))
+num2 = int(input("please enter your 2nd number: "))
+
+for i in range(num1, num2):
+    print(i)
+
+for i in range(50, 101):
+    print(i)
+
+for i in range (100, 50): 
+    print(i)       #es kargad ver gavige ukugma rogor dameprinta
+
+result = 0
+for i in range(0, 51):
+    result = result + i
+
+num3 = int(input("please enter your 3rd number: "))
+
+for i in range(0, num3):
+    print(i)
+
+age = int(input("please enter your age: "))
+age2 = age + 10
+
+for i in range(age , age2):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
