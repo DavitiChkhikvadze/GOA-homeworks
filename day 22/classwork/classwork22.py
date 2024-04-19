70be921e0f3c89b44911c02b371c1763caaf6a27
DavitiChkhikvadze <data.poshki2020@gmail.com>
2024-04-19 15:22:28 +0400

    new 22 commit

---
+++ day 22/classwork/classwork22.py
@@ -0,0 +1,49 @@
+# list = []
+# start = 0
+# end = 20
+# step = 2
+
+# while start < end:
+#     list.append(start)
+#     start = start + step
+
+# print(list)
+
+# nums = [1, 38, 234, 235, 123, 538, 47]
+# list1 = []
+# start1 = 0
+# end1 = 4
+# step1 = 1
+
+# while start1 < end1:
+#     list1.append(nums[start1])
+#     start1 = start1 + step1
+# print(list1)
+
+# string = "goaisbest"
+# print(string[1:4])
+
+list = []
+start = 5
+end = -1
+step = -1
+
+while start > end:
+    list.append(start)
+    start += step
+
+print(list)
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_list = []
+
+start1 = 2
+end1 = 0
+step1 = -1
+
+while start1 > end1:
+    new_list.append(nums[start1])
+    start += step1
+
+print(new_list)
